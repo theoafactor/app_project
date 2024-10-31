8ec20b207da651dc793a8adb79888b410d54d4dd
Olu Adeyemo <theoafactor@gmail.com>
2024-11-01 00:47:26 +0100

    Added artefact

--- test.sh
+++ test.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-if [ -x "$( command -v docker )" ]
+# if [ -x "$( command -v docker )" ]
+# then
+#     echo "Docker is installed.. moving on"
+# else
+#     echo "Docker is not installed. Installing it ..."
+# fi
+
+
+folder_name="backups"
+
+if [[ -d $folder_name ]]
 then
-    echo "Docker is installed.. moving on"
+    sleep 3
+    echo "..."    
+    echo "Folder exists .. moving on"
 else
-    echo "Docker is not installed. Installing it ..."
+    echo "Folder does not exist"
+    echo "Creating it ..."
+    sleep 3
+    mkdir $folder_name
+    echo "Folder created!"
 fi
